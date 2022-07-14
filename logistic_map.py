c3bbabc9892f6fc965d54f50e4e40fa84d41100c
seyunbaek <76844669+seyunbaek@users.noreply.github.com>
2022-07-14 19:49:45 +0800

    Add files via upload

---
+++ logistic_map.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+rs = np.linspace(1,4,100000)
+N = 500
+x = 0.1 + np.zeros(N)
+endcap = np.arange(int(N*0.9),N)
+
+rates = []
+uniques = []
+
+for ri in range(len(rs)):
+    for n in range(N-1):
+        x[n+1] = rs[ri]*x[n]*(1-x[n])
+    u = np.unique(x[endcap])
+    r = rs[ri]*np.ones(len(u))
+    for i in u:
+        rates.append(rs[ri])
+        uniques.append(i)
+
+with open('logistic_map.csv','w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Growth Rate","Fixed Value"])
+    for i in range(len(uniques)):
+        writer.writerow([rates[i],uniques[i]])
+
+# plt.style.use('dark_background')
+# plt.figure(figsize=(16, 9))
+# plt.axis('off')
+# plt.plot(rates,uniques,'.',color='white',markersize=0.03)
+# plt.savefig("Logistic Map.png",dpi=1200)
+# plt.show()
